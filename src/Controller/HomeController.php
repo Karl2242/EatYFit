5098629522d14486182173677841a2865660a079
Karl2242 <kchikhicloud@gmail.com>
2025-04-23 10:50:44 +0200

    new page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\InfoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,4 +22,6 @@ final class HomeController extends AbstractController
             'info' => $info,
         ]);
     }
+
+    
 }
